75be8b528738141581322c5808730a4d5b425320
Stephen Ndiananie <stevendi23@yahoo.com>
2021-01-25 15:45:37 +0000

    ClientApp update

--- BugsTrackerAuthUI/Codewithsteve.Microservices.BugsTracker.Auth.UI/ClientApp/README.md
+++ BugsTrackerAuthUI/Codewithsteve.Microservices.BugsTracker.Auth.UI/ClientApp/README.md
@@ -1,4 +1,4 @@
-# Codewithsteve.BugsTracker.Auth.UI
+# Codewithsteve.BugsTracker.Angular.Core.UI
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
 
--- BugsTrackerAuthUI/Codewithsteve.Microservices.BugsTracker.Auth.UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ BugsTrackerAuthUI/Codewithsteve.Microservices.BugsTracker.Auth.UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -7,10 +7,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
+  public clients: Client[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
       this.forecasts = result;
+    }, error => console.error(error));
+
+    http.get<Client[]>(baseUrl + 'client').subscribe(result => {
+      this.clients = result;
     }, error => console.error(error));
   }
 }
@@ -20,4 +25,10 @@ interface WeatherForecast {
   temperatureC: number;
   temperatureF: number;
   summary: string;
+}
+
+interface Client {
+  id: string;
+  dateCreated: string;
+  name: string;
 }
